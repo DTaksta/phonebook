2395288cd1524a59fbab422d2932919c0af1959e
DTaksta <deontakpuie@gmail.com>
2019-11-10 19:48:21 +0200

    dynamic form fields development
    dynamic form fields development

--- application/views/phonebook/edit.php
+++ application/views/phonebook/edit.php
@@ -4,6 +4,9 @@
 	<meta charset="UTF-8">
 	<title>Phonebook</title>
 	<link href="<?= base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet">
+	<!--dynamic form fields.-->
+	<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet" id="bootstrap-css">
+	<link href="<?= base_url('assets/css/dynamicformfields.css') ?>" rel="stylesheet">
 </head>
 <body>
 	<div class="wrapper">
@@ -24,15 +27,47 @@
                 	<div class="form-group">
                 		<label for="name">Name</label>
                 		<input type="text" name="name" id="name" class="form-control" value="<?= $record->name ?>">
-                	</div>
-                	<div class="form-group">
+					</div>
+					<!-- Dynamic Form Fields BEGIN. -->
+					<div class="container">
+						<div class="row">
+							<input type="hidden" name="count" value="1" />
+							<div class="control-group" id="fields">
+								<label class="control-label" for="field1">Nice Multiple Form Fields</label>
+								<div class="controls" id="profs"> 
+									<form class="input-append">
+										<div id="field"><input autocomplete="off" class="input" id="field1" name="prof1" type="text" placeholder="Type something" data-items="8"/><button id="b1" class="btn add-more" type="button">+</button></div>
+									</form>
+								<br>
+								<small>Press + to add another form field :)</small>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="container">
+						<div class="row">
+							<input type="hidden" name="count" value="1" />
+							<div class="control-group" id="fields">
+								<label class="control-label" for="field1">Nice Multiple Form Fields</label>
+								<div class="controls" id="profs"> 
+									<form class="input-append">
+										<div id="field"><input autocomplete="off" class="input" id="field1" name="prof1" type="text" placeholder="Type something" data-items="8"/><button id="b1" class="btn add-more" type="button">+</button></div>
+									</form>
+								<br>
+								<small>Press + to add another form field :)</small>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- Dynamic Form Fields END. -->
+                	<!-- <div class="form-group">
                 		<label for="email">Email</label>
                 		<input type="email" name="email" id="email" class="form-control" value="<?= $record->email ?>">
                 	</div>
                 	<div class="form-group">
                 		<label for="phone">Phone</label>
                 		<input type="text" name="phone" id="phone" class="form-control" value="<?= $record->phone ?>">
-                	</div>
+                	</div> -->
                 	<button type="submit" class="btn btn-default">Submit</button>
 					<a href="<?= site_url('phonebook/index') ?>" class="btn btn-danger">Cancel</a>
                 </form>
@@ -41,6 +76,11 @@
 	</div>
 
     <script src="<?= base_url('assets/js/jquery-2.1.4.min.js') ?>"></script>
-    <script src="<?= base_url('assets/js/bootstrap.min.js') ?>"></script>
+	<script src="<?= base_url('assets/js/bootstrap.min.js') ?>"></script>
+	<!--dynamic form fields-->
+	<script src="<?= base_url('assets/js/jquery-2.1.4.min.js') ?>"></script>
+	<script src="<?= base_url('assets/js/dynamicformfields.js') ?>"></script>
+	<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
+	<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 </body>
 </html>
